054b9117eb796346f99e3d8f5c6d809e20a51b0c
Rezlen <soheilyreza@yahoo.com>
2022-05-03 07:20:56 +0100

    files added

---
+++ main.js
@@ -0,0 +1,1 @@
+// In this project, you’ll build a message generator program that outputs a new, random message every time a user runs the program. Your program should showcase basic JavaScript syntax and programming concepts.*//
